ce86ff6c875b53412915bd8acf89262f858315e2
Luis Felipe Martínez(Kispat) <martinezbluisf@gmail.com>
2026-04-17 16:51:04 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,38 +1,29 @@
 import argparse
 import json
-import logging
-import os
-from organizer.core import organizar_archivos
+from organizer.core import organizar_archivos, limpiar_duplicados, organizar_por_fecha
 
-def setup_logging():
-    logging.basicConfig(
-        filename="app.log",
-        level=logging.INFO,
-        format="%(asctime)s - %(levelname)s - %(message)s"
-    )
-
-def load_config(path="config.json"):
-    if not os.path.exists(path):
-        raise FileNotFoundError("config.json not found")
-    
-    with open(path, "r") as f:
+def load_config():
+    with open("config.json") as f:
         return json.load(f)
 
 def main():
-    parser = argparse.ArgumentParser(description="File Organizer CLI Tool")
-    parser.add_argument("--path", required=True, help="Target directory path")
+    parser = argparse.ArgumentParser(description="File Organizer CLI")
+
+    parser.add_argument("--path", required=True)
+    parser.add_argument("--mode", choices=["organize", "clean", "date"], default="organize")
 
     args = parser.parse_args()
+    config = load_config()
 
-    setup_logging()
-
-    try:
-        config = load_config()
+    if args.mode == "organize":
         organizar_archivos(args.path, config)
-        print("✅ Organization completed successfully")
-    except Exception as e:
-        logging.error(str(e))
-        print(f"❌ Error: {e}")
 
-if __name__ == "__main__":
+    elif args.mode == "clean":
+        limpiar_duplicados(args.path)
+
+    elif args.mode == "date":
+        organizar_por_fecha(args.path)
+
+    print("✅ Done")
+
     main()
