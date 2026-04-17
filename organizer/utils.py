00d4ed234ed062717688eb084f8eb1561e866dae
Luis Felipe Martínez(Kispat) <martinezbluisf@gmail.com>
2026-04-17 16:48:47 -0500

    Update utils.py

--- organizer/utils.py
+++ organizer/utils.py
@@ -1,1 +1,28 @@
+import argparse
+import json
+from organizer.core import organizar_archivos, limpiar_duplicados, organizar_por_fecha
+
+def load_config():
+    with open("config.json") as f:
+        return json.load(f)
+
+def main():
+    parser = argparse.ArgumentParser(description="File Organizer CLI")
+
+    parser.add_argument("--path", required=True)
+    parser.add_argument("--mode", choices=["organize", "clean", "date"], default="organize")
+
+    args = parser.parse_args()
+    config = load_config()
+
+    if args.mode == "organize":
+        organizar_archivos(args.path, config)
+
+    elif args.mode == "clean":
+        limpiar_duplicados(args.path)
+
+    elif args.mode == "date":
+        organizar_por_fecha(args.path)
+
+    print("✅ Done")
 
