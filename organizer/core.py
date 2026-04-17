b6c5a4095222073fb1239fd4c6d21dc7db4019bb
Luis Felipe Martínez(Kispat) <martinezbluisf@gmail.com>
2026-04-17 16:23:22 -0500

    Update core.py

--- organizer/core.py
+++ organizer/core.py
@@ -1,1 +1,17 @@
+import os
+import shutil
+
+def organizar_archivos(ruta):
+    for archivo in os.listdir(ruta):
+        ruta_completa = os.path.join(ruta, archivo)
+
+        if os.path.isfile(ruta_completa):
+            extension = archivo.split('.')[-1]
+
+            carpeta_destino = os.path.join(ruta, extension)
+
+            if not os.path.exists(carpeta_destino):
+                os.makedirs(carpeta_destino)
+
+            shutil.move(ruta_completa, os.path.join(carpeta_destino, archivo))
 
