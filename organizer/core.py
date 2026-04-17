21c8f909d86805dfd9c732ed888496af30f3feb4
Luis Felipe Martínez(Kispat) <martinezbluisf@gmail.com>
2026-04-17 16:37:19 -0500

    Update core.py

--- organizer/core.py
+++ organizer/core.py
@@ -1,17 +1,31 @@
 import os
 import shutil
+import logging
 
-def organizar_archivos(ruta):
+def get_category(extension, config):
+    for category, extensions in config.items():
+        if extension.lower() in extensions:
+            return category
+    return "others"
+
+def organizar_archivos(ruta, config):
+    if not os.path.exists(ruta):
+        raise ValueError("Invalid path")
+
     for archivo in os.listdir(ruta):
         ruta_completa = os.path.join(ruta, archivo)
 
         if os.path.isfile(ruta_completa):
             extension = archivo.split('.')[-1]
 
-            carpeta_destino = os.path.join(ruta, extension)
+            categoria = get_category(extension, config)
+            carpeta_destino = os.path.join(ruta, categoria)
 
             if not os.path.exists(carpeta_destino):
                 os.makedirs(carpeta_destino)
 
-            shutil.move(ruta_completa, os.path.join(carpeta_destino, archivo))
+            destino_final = os.path.join(carpeta_destino, archivo)
 
+            shutil.move(ruta_completa, destino_final)
+
+            logging.info(f"Moved: {archivo} → {categoria}")
